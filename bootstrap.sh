31d32a3dab631e39fd9684f3b8adb892a3f52c90
Mike Carr <mcarr67@gmail.com>
2019-09-08 11:37:18 -0800

    fix script; add bashrc file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="bashrc vimrc zshrc "    # list of files/folders to symlink in homedir
+#files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
